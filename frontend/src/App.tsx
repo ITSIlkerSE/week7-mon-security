70b35f8e54f2273a238d48bad91f2d5af8a9afcc
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-10-13 10:29:22 +0200

    Add welcome message with button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+    const [welcomeMessage, setWelcomeMessage] = useState("")
+
+    function fetchWelcomeMessage (){
+        axios.get("/api/hello")
+            .then(response => {return response.data})
+            .then(data => setWelcomeMessage(data))
+
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <p>{welcomeMessage}</p>
+          <button onClick={fetchWelcomeMessage}>Say Hello!</button>
+
       </header>
     </div>
   );
