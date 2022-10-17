51b2e7026347fe595f0d2093ea6b2b5bd613cc0d
ITSIlkerSE <itsilkerse@gmail.com>
2022-10-17 17:10:35 +0200

    Add ControllerAdvisor

---
+++ backend/src/main/java/com/example/backend/controller/ControllerAdvisor.java
@@ -0,0 +1,37 @@
+package com.example.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ControllerAdvisor {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<Object> handleConflict(NoSuchElementException ex){
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Element not found Error:" + ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<Object> handleConflict(IllegalArgumentException ex){
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Given element not proccesable Error::" + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> handleConflict(Exception ex){
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Server Error:" + ex.getMessage());
+    }
+
+}
